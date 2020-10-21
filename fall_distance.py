ee49231730a3f204760407bb8af7b36a039e45ca
jamarrhill <hilljama@oregonstate.edu>
2020-10-20 23:17:28 -0800

    Assignment 4.a

---
+++ fall_distance.py
@@ -0,0 +1,13 @@
+# Name: Jamar Hill
+# Date: 10/20/2020
+# Description: Assignment 4.a
+
+def fall_distance(t):
+
+   g = 9.8
+   dist = ( 1 / 2 ) * g * ( t ** 2 )
+   return dist
+
+dist = fall_distance(3.2)
+print(dist)
+
